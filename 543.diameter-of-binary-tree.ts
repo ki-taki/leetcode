7f3dd99559154374103d36e956779412e05ee9d2
ki-taki <kksw10@icloud.com>
2024-10-17 23:42:07 +0900

    543. Diameter of Binary Tree

--- 543.diameter-of-binary-tree.ts
+++ 543.diameter-of-binary-tree.ts
@@ -20,7 +20,19 @@
  */
 
 function diameterOfBinaryTree(root: TreeNode | null): number {
-    
-};
-// @lc code=end
+  let res = [0];
 
+  function dfs(root: TreeNode | null): number {
+    if (!root) return -1;
+    let left = dfs(root.left);
+    let right = dfs(root.right);
+
+    res[0] = Math.max(res[0], 2 + left + right);
+
+    return 1 + Math.max(left, right);
+  }
+
+  dfs(root);
+  return res[0];
+}
+// @lc code=end
