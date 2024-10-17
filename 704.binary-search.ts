d94ddaa3f69e35a92228076140b0e45477a12029
ki-taki <kksw10@icloud.com>
2024-10-17 19:51:01 +0900

    704. Binary Search

---
+++ 704.binary-search.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=704 lang=typescript
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+function search(nums: number[], target: number): number {
+  let l = 0,
+    r = nums.length - 1;
+
+  while (l <= r) {
+    var m = l + Math.floor((r - l) / 2);
+    if (nums[m] > target) {
+      r = m - 1;
+    } else if (nums[m] < target) {
+      l = m + 1;
+    } else {
+      return m;
+    }
+  }
+
+  return -1;
+}
+
+// @lc code=end
