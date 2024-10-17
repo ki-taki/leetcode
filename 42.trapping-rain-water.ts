dc8a25774454cb82836537e84834dac9153ee414
ki-taki <kksw10@icloud.com>
2024-10-17 19:23:24 +0900

    42. Trapping Rain Water

---
+++ 42.trapping-rain-water.ts
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=42 lang=typescript
+ *
+ * [42] Trapping Rain Water
+ */
+
+// @lc code=start
+function trap(height: number[]): number {
+  if (!height) return 0;
+
+  let l = 0;
+  let r = height.length - 1;
+  let leftMax = height[l];
+  let rightMax = height[r];
+  let res = 0;
+
+  while (l < r) {
+    if (leftMax < rightMax) {
+      l += 1;
+      leftMax = Math.max(leftMax, height[l]);
+      res += leftMax - height[l];
+    } else {
+      r -= 1;
+      rightMax = Math.max(rightMax, height[r]);
+      res += rightMax - height[r];
+    }
+  }
+
+  return res;
+}
+// @lc code=end
