49a28a0de06f8e2443140adc8e9ae111a4acc9c7
ki-taki <kksw10@icloud.com>
2024-10-18 00:18:03 +0900

    206. Reverse Linked List

---
+++ 206.reverse-linked-list.ts
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=206 lang=typescript
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let prev: ListNode | null = null;
+
+  while (head) {
+    let ele = head.next;
+    head.next = prev;
+    prev = head;
+    head = ele;
+  }
+  return prev;
+}
+// @lc code=end
