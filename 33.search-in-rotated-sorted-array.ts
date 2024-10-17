8d60ba5f8189f37a55d81568d2832eb5909695f4
ki-taki <kksw10@icloud.com>
2024-10-17 20:05:14 +0900

    33. Search in Rotated Sorted Array

---
+++ 33.search-in-rotated-sorted-array.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=33 lang=typescript
+ *
+ * [33] Search in Rotated Sorted Array
+ */
+
+// @lc code=start
+function search(nums: number[], target: number): number {
+  let left: number = 0;
+  let right: number = nums.length - 1;
+
+  while (left <= right) {
+    let midIdx: number = Math.floor((left + right) / 2);
+    if (nums[midIdx] === target) return midIdx;
+
+    if (nums[left] <= nums[midIdx]) {
+      if (nums[left] <= target && target <= nums[midIdx]) right = midIdx - 1;
+      else left = midIdx + 1;
+    } else {
+      if (nums[midIdx] <= target && target <= nums[right]) left = midIdx + 1;
+      else right = midIdx - 1;
+    }
+  }
+  return -1;
+}
+
+// @lc code=end
