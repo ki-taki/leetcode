c606637362406556ccca430b338251e881fbd06e
ki-taki <kksw10@icloud.com>
2024-10-17 19:33:56 +0900

    22. Generate Parentheses

---
+++ 22.generate-parentheses.ts
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=22 lang=typescript
+ *
+ * [22] Generate Parentheses
+ */
+
+// @lc code=start
+function generateParenthesis(n: number): string[] {
+  const stack: string[] = [];
+  const res: string[] = [];
+
+  function backtrack(openN: number, closedN: number) {
+    if (openN === n && closedN === n) {
+      res.push(stack.join(""));
+      return;
+    }
+
+    if (openN < n) {
+      stack.push("(");
+      backtrack(openN + 1, closedN);
+      stack.pop();
+    }
+
+    if (closedN < openN) {
+      stack.push(")");
+      backtrack(openN, closedN + 1);
+      stack.pop();
+    }
+  }
+
+  backtrack(0, 0);
+
+  return res;
+}
+// @lc code=end
