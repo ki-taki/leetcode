535dfe13dbb2cc405180cffba883732ea6c00853
ki-taki <kksw10@icloud.com>
2024-10-18 00:10:07 +0900

    105. Construct Binary Tree from Preorder and Inorder Traversal

---
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.ts
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=105 lang=typescript
+ *
+ * [105] Construct Binary Tree from Preorder and Inorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
+    function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
+        if (!preorder.length || !inorder.length) {
+            return null;
+        }
+    
+        let root = new TreeNode(preorder[0]);
+        let mid = inorder.indexOf(preorder[0]);
+    
+        root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid));
+        root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1));
+        return root;
+    }
+// @lc code=end
+
