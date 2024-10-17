6e217b0b856fc517a4984eff3e2b1da3165eebe0
ki-taki <kksw10@icloud.com>
2024-10-17 23:41:28 +0900

    543. Diameter of Binary Tree

---
+++ 543.diameter-of-binary-tree.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=543 lang=typescript
+ *
+ * [543] Diameter of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function diameterOfBinaryTree(root: TreeNode | null): number {
+    
+};
+// @lc code=end
+
