865fed624d97cf4bdb8e9671f3aafd1f81640632
ki-taki <kksw10@icloud.com>
2024-10-18 11:44:56 +0900

    78. Subsets

---
+++ 78.subsets.ts
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=78 lang=typescript
+ *
+ * [78] Subsets
+ */
+
+// @lc code=start
+function subsets(nums: number[]): number[][] {
+  let res: number[][] = [];
+  let subset: number[] = [];
+  function dfs(i: number) {
+    if (i >= nums.length) {
+      res.push(subset.slice());
+      return;
+    }
+
+    subset.push(nums[i]);
+    dfs(i + 1);
+
+    subset.pop();
+    dfs(i + 1);
+  }
+
+  dfs(0);
+
+  return res;
+}
+
+// @lc code=end
