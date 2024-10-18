a21ac3eb51b5300444e5c875f4a4b4c4e4e7978e
ki-taki <kksw10@icloud.com>
2024-10-18 14:01:03 +0900

    621. Task Scheduler

---
+++ 621.task-scheduler.ts
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=621 lang=typescript
+ *
+ * [621] Task Scheduler
+ */
+
+// @lc code=start
+function leastInterval(tasks: string[], n: number): number {
+  const charMap = new Map();
+  let maxCharCount = 0;
+  let maxChar = tasks[0];
+
+  for (let char of tasks) {
+    charMap.set(char, (charMap.get(char) || 0) + 1);
+    if (charMap.get(char) > maxCharCount) {
+      maxCharCount = charMap.get(char);
+      maxChar = char;
+    }
+  }
+
+  let idleCount = (maxCharCount - 1) * n;
+
+  charMap.forEach((count, char) => {
+    // 'return' inside forEach() serve as 'continue'
+    if (char === maxChar) {
+      return;
+    }
+    if (count === maxCharCount) {
+      idleCount -= count - 1;
+    } else {
+      idleCount -= count;
+    }
+  });
+
+  if (idleCount <= 0) {
+    return tasks.length;
+  }
+  return tasks.length + idleCount;
+}
+
+// @lc code=end
